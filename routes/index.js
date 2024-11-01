a8887ee2ccf2187d4883fc06d328e40477f17110
3102005 <cristhianpadron2005@gmail.com>
2024-10-31 21:46:13 -0400

    1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('hola mundo, Estefani Jeannielys Gss465sonzalez Gonzalez C.I: 32.057.736 seccion: 4');
+  res.render('index' , { title: 'Express'});
 });
 
 module.exports = router;
